f5dfa344792457d5b9db22d5e091cf11f91ebdf7
Matthew <MatthewMCSClub@gmail.com>
2024-04-17 01:24:00 -0700

    Added Basic Functionality

--- Workspace.py
+++ Workspace.py
@@ -13,8 +13,6 @@
 # geeksforgeeks.org
 # docs.python.org
 import random
-name = ""
-lb = 0
 class Item:
     def __init__(self, commons, uncommons, rares, name, lb):
         self.commons = commons
@@ -24,31 +22,50 @@ class Item:
         self.lb = lb
       
     def roll(self):
-        roll_val = random.randint(1, 101) + self.lb
-        if roll_val <= 30:
+        roll_val = (random.randrange(self.lb, 1001)/100)
+        print(roll_val)
+        if roll_val <= 5:
             result = random.choices(self.commons, weights=(40, 25, 20, 10, 5), k=1)[0]
             name = result[0]
             lb = result[1]
             print(f'You recieved {name} and it has a luck boost of {lb}!\n')
-        elif roll_val > 30 and roll_val <= 50:
+        elif roll_val > 5 and roll_val <= 7:
             result = random.choices(self.uncommons, weights=(60, 20, 10, 6, 4), k=1)[0]
             name = result[0]
             lb = result[1]
             print(f'You recieved {name} and it has a luck boost of {lb}!\n')
+        elif roll_val > 7 and roll_val <= 8:
+            print("You recieved a hypothetical rare!")
+            result = None
+        elif roll_val > 8 and roll_val <= 8.5:
+            print("You recieved a hypothetical epic!")
+            result = None
+        elif roll_val > 8.5 and roll_val <= 8.8:
+            print("You recieved a hypothetical legendary!")
+            result = None
+        elif roll_val > 8.8 and roll_val <= 9:
+            print("You recieved a hypothetical mythic!")
+            result = None
         else:
-            print("You did not recieve anything LMAO\n")
-            pass
+            print("You did not recieve anything! (If you see this message, this program must be in testing!)\n")
+            result = None
+        return result
     def equip(self, name, lb):
         self.name = name
         self.lb = lb
-commons = [["Pencil", 0.5], ["Eraser", 1], ["Notebook", 2], ["Paperclip", 2.5], ["Binder", 5]]
+commons = [["Pencil", 1], ["Eraser", 2], ["Notebook", 3], ["Paperclip", 4], ["Binder", 5]]
 uncommons = [["Computer Mouse", 6], ["Keyboard", 8], ["Monitor", 10], ["Laptop", 12], ["Phone", 15]]
 rares = []
 Wand = Item(commons, uncommons, rares, "Placeholder", 0)
-for i in range(100):
-    Wand.roll()
-    Wand.equip(name, lb)
-    print(Wand.name, Wand.lb)
+while True:
+    choice = str(input("What to do?"))
+    if choice == "roll":
+        result = Wand.roll()
+        if result:
+            name, lb = result
+            if Wand.lb < lb:
+                Wand.equip(name, lb)
+    print(f'Current Equipped Item: {Wand.name} | Luck Boost (Roll + Luck Boost Value): {Wand.lb}')
 
 
 
